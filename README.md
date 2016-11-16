9feea2b7d0ad89141a1f0e7e02c0fc5608c504aa
Charie Smith <charlie.smith1@sainsburys.co.uk>
2016-11-16 17:01:25 +0000

    Add test content

---
+++ README.md
@@ -0,0 +1,11 @@
+# Go Excercise
+
+Build a program that does the following things:
+
+- Prints all employees who work in development that work on 5th floor
+- Prints all employees that have Charlie as their manager.
+- Prints all employees that work in Product
+
+Write your program in main.go
+
+Your program must build and run successfully
--- main.go
+++ main.go
@@ -1,8 +1,69 @@
 package main
 
-import "fmt"
-
 func main() {
-	fmt.Println("Hello World!")
+	// Implment your solution here
 }
 
+type details struct {
+	age     int
+	dept    string
+	floor   int
+	manager string
+}
+
+var People = map[string]details{
+	"charlie": details{
+		age:     22,
+		dept:    "development",
+		manager: "Fraser",
+		floor:   5,
+	},
+	"john": details{
+		age:     34,
+		dept:    "Product",
+		manager: "Chris",
+		floor:   5,
+	},
+	"bob": details{
+		age:     24,
+		dept:    "Development",
+		manager: "Charlie",
+		floor:   5,
+	},
+	"chris": details{
+		age:     34,
+		dept:    "Development",
+		manager: "Fraser",
+		floor:   3,
+	},
+	"nadim": details{
+		age:     44,
+		dept:    "Development",
+		manager: "Fraser",
+		floor:   3,
+	},
+	"josh": details{
+		age:     25,
+		dept:    "Development",
+		manager: "Jai",
+		floor:   5,
+	},
+	"micheal": details{
+		age:     28,
+		dept:    "Development",
+		manager: "Charlie",
+		floor:   5,
+	},
+	"alex": details{
+		age:     32,
+		dept:    "Development",
+		manager: "Charlie",
+		floor:   5,
+	},
+	"andrew": details{
+		age:     44,
+		dept:    "Development",
+		manager: "Charlie",
+		floor:   3,
+	},
+}
