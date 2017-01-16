206a0c3b026b5a24db8341b1b3fa4a421f46ee84
sugan2111 <sugan2111@gmail.com>
2017-01-16 14:56:28 +0000

    Webservice exercise

---
+++ lesson1/exercise-3/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"net/http"
+)
+
+type Response struct {
+	Code   int    `json:"code"`
+	Result string `json:"result"`
+}
+
+func showReponse(
+res string,
+w http.ResponseWriter,
+code int,
+) {
+	if code == 500 {
+		res = "Bad request"
+	}
+	r := Response{
+		Code:   code,
+		Result: res,
+	}
+	js, err := json.Marshal(r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(code)
+	w.Write(js)
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	// Build the response struct
+	name := r.FormValue("name")
+	code := 200
+	res := ""
+	if len(name) == 0 {
+		code = 422
+	} else {
+		res = fmt.Sprintf("%s %s", "hello", name)
+	}
+
+	showReponse(res, w, code)
+}
+
+func main() {
+	port := flag.String("port", "8000", "Server port")
+	flag.Parse()
+
+	http.HandleFunc("/", hello)
+
+	// Starts the web server
+	http.ListenAndServe(":"+*port, nil)
+}
